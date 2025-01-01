9f2a0d6c003a5037c90795ae88e78bef68010d39
TechEdzprogrammer <113817954+TechEdzprogrammer@users.noreply.github.com>
2025-01-01 15:41:22 +0800

    Loading screen
    Added animation for loading screen before the fetch data is rendered.

--- app.js
+++ app.js
@@ -10,7 +10,8 @@ async function shortURL(){
         const fetchUrlApi = await fetch(`https://www.shareaholic.com/v2/share/shorten_link?apikey=${APIKey}&url=${inputURL.value}`);
         if(!fetchUrlApi.ok){
             throw new Error(`Parameter error: ${fetchUrlApi.status}`);
-        };
+        }
+        loadingScreen();
         const data = await fetchUrlApi.json();
         shortenedURL.value = data.data;
     } catch (error) {
@@ -23,6 +24,18 @@ async function shortURL(){
 
 function copyShortUrl(){
     navigator.clipboard.writeText(shortenedURL.value);
+}
+
+function loadingScreen(){
+    const overlay = document.querySelector('.loading-overlay');
+    
+    // Show the loading overlay
+    overlay.style.display = 'flex';
+    
+    // Hide it after 3 seconds
+    setTimeout(() => {
+      overlay.style.display = 'none';
+    }, 1000); // 3000ms = 3 seconds
 }
 
 shortBtn.addEventListener('click',shortURL);
